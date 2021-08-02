f967e055e834fc3847e5771bf9d8a28a69869d1a
A00233642 <84159018+A00233642@users.noreply.github.com>
2021-08-02 12:04:00 -0400

    created classes

---
+++ TrackingApplication/Pages/Data/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackingApplication.Pages.Data
+{
+    public class Book
+    {
+        [Key]
+        public String ISBN { get; set; }
+
+        public String Title { get; set; }
+
+        public int Category { get; set; }
+
+        public String Author { get; set; }
+    }
+}
