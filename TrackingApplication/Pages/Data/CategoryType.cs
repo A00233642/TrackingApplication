677b6f8249341760eca14f8a8fe4c0dde91c1583
A00233642 <84159018+A00233642@users.noreply.github.com>
2021-08-02 12:05:31 -0400

    CategoryType Class

--- TrackingApplication/Pages/Data/CategoryType.cs
+++ TrackingApplication/Pages/Data/CategoryType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace TrackingApplication.Pages.Data
 {
     public class CategoryType
     {
+        [Key]
+        public String Type { get; set; }
+
+        [StringLength(128, MinimumLength = 1)]
+        public String Name { get; set; }
     }
 }
