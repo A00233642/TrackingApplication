05deaaedf423fba8d741977527777be4c5ac8124
A00233642 <84159018+A00233642@users.noreply.github.com>
2021-08-02 20:57:24 -0400

    Scaffold created

---
+++ TrackingApplication/Pages/CategoryPages/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TrackingApplication.Pages.Data;
+using TrackingApplication.Pages.Data.Context;
+
+namespace TrackingApplication.Pages.CategoryPages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TrackingApplication.Pages.Data.Context.StoreContext _context;
+
+        public DetailsModel(TrackingApplication.Pages.Data.Context.StoreContext context)
+        {
+            _context = context;
+        }
+
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FirstOrDefaultAsync(m => m.NameToken == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
